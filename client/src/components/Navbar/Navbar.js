1c193bc580ffe46afb901127050c02ab0cea248c
Danielle Rotem <rotemdanielle@gmail.com>
2025-09-04 17:09:17 +0300

    ok

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  const [show, setShow] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  const controlNavbar = () => {
-    if (window.scrollY > lastScrollY) {
-      // scroll down → hide
-      setShow(false);
-    } else {
-      // scroll up → show
-      setShow(true);
-    }
-    setLastScrollY(window.scrollY);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", controlNavbar);
-    return () => window.removeEventListener("scroll", controlNavbar);
-  }, [lastScrollY]);
-
   return (
-    <nav
-      className={styles.navbar}
-      style={{ top: show ? "0" : "-100px" }} // moves navbar out of view
-    >
+    <nav className={styles.navbar}>
       <img src="/project-logo.png" alt="Logo" className={styles.logo} />
-      <div className={styles.navLinks}>
+      <div className={styles.centerNav}>
         <Link to="/" className={styles.navLink}>Home</Link>
         <Link to="/new" className={styles.navLink}>New Recipe</Link>
       </div>
+      <div className={styles.rightNav}></div> {/* optional for spacing */}
     </nav>
   );
 };
