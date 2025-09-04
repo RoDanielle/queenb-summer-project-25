c8f9ac5dd67d11e1bba67b038c3ee41ceab31518
Danielle Rotem <rotemdanielle@gmail.com>
2025-09-04 18:45:52 +0300

    made the logo the new home button - clicking on logo redirects to home page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,9 +5,9 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <img src="/project-logo.png" alt="Logo" className={styles.logo} />
-      <div className={styles.centerNav}>
-        <Link to="/" className={styles.navLink}>Home</Link>
+        <Link to="/">
+          <img src="/project-logo.png" alt="Logo" className={styles.logo} />
+        </Link>      <div className={styles.centerNav}>
         <Link to="/new" className={styles.navLink}>New Recipe</Link>
       </div>
       <div className={styles.rightNav}></div> {/* optional for spacing */}
